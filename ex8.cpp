aa1f89750f9ec1d34d6003429f8f781a921f4b74
Benjamin Kocher <benjamin.kocher@hes-so.ch>
2023-11-06 08:43:00 +0100

    ex8 et ex9

--- ex8.cpp
+++ ex8.cpp
@@ -7,7 +7,16 @@ using namespace std;
 // écrire une fonction nb_lignes qui compte le nombre lignes
 // d'un fichier dont le nom est passé en paramètre. Il doit retourner
 // une valeur négative en cas d'impossibilité d'ouvrir le fichier
-
+int nb_lignes(string const& filename){
+    ifstream file(filename);
+    if(!file) return -1;
+    int cnt = 0;
+    string s;
+    while(getline(file, s)){
+        ++cnt;
+    }
+    return cnt;
+}
 int main() {
    cout << "Entrez le nom du fichier : " << flush;
    string filename; cin >> filename;
--- ex9.cpp
+++ ex9.cpp
@@ -7,8 +7,22 @@ using namespace std;
 // écrire une fonction nb_char_in_file(filename, c) qui compte le nombre
 // de fois que la caractère c apparait dans le fichier. Il doit retourner
 // une valeur négative en cas d'impossibilité d'ouvrir le fichier
-
+int nb_char_in_file(string const& filename, int c){
+    ifstream file(filename);
+    if (!file)
+        return -1;
+    int cnt = 0;
+    char d;
+    while(file.get(d)){
+        if(d==c)
+            ++cnt;
+    }
+    return cnt;
+}
 int main() {
+    ofstream file_out("test.txt", ios::out);
+    file_out << "premiere ligne avec c comme test\n";
+    file_out.close();
    cout << "Entrez le nom du fichier : " << flush;
    string filename; cin >> filename;
    cout << "Entrez le caractère : " << flush;
